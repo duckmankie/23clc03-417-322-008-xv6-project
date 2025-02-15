1dd5cb3946710b0fd6ab555debd9c554660c17d3
DuckMankie <dhdmanh23@clc.fitus.edu.vn>
2025-02-15 14:34:01 +0700

    hoang bach update find.c

--- user/primes.c
+++ user/primes.c
@@ -2,63 +2,86 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-#define MAX 36
-#define FIRST_PRIME 2
-
-int generate_natural() {
-	int out_pipe[2];
+// Hàm này sẽ tạo pipe và write vào pipe từ số 2 đến 279 và trả về p[0] (read) của pipe
+int generateNumbers() {
+	int p[2];
 	
-	pipe(out_pipe);
+	pipe(p);
 
-	if (!fork()) {
-		for (int i = FIRST_PRIME; i < MAX; i++) {
-			write(out_pipe[1], &i, sizeof(int));
+	if (fork() == 0) {
+		for (int i = 2; i < 280; i++) {
+			write(p[1], &i, sizeof(int));
 		}
-		close(out_pipe[1]);
+		close(p[1]);
 
 		exit(0);
 	}
 
-	close(out_pipe[1]);
+	close(p[1]);
 
-	return out_pipe[0];
+	return p[0]; // đây là p[0] của tiến trình cha nha
 }
 
-int prime_filter(int in_fd, int prime) 
+// Hàm này sẽ đọc từ in_fd và lọc ra các số không chia hết cho prime và trả về p[0] (read) của pipe
+// Hàm này sẽ sẻ dụng descriptor của pipe cũ từ hàm generateNumbers và lọc rồi pass vào pipe mới khác
+int filterPrime(int in_fd, int prime) 
 {
 	int num;
-	int out_pipe[2];
+	int p[2];
 
-	pipe(out_pipe);
+	pipe(p);
 
-	if (!fork()) {
+	if (fork() == 0) {
 		while (read(in_fd, &num, sizeof(int))) {
-			if (num % prime) {
-				write(out_pipe[1], &num, sizeof(int));
+			if (num % prime != 0) {
+				write(p[1], &num, sizeof(int));
 			}
 		}
 		close(in_fd);
-		close(out_pipe[1]);
+		close(p[1]);
 		
 		exit(0);
 	}
 
 	close(in_fd);
-	close(out_pipe[1]);
+	close(p[1]);
 
-	return out_pipe[0];
+	return p[0];
 }
 
 int main(int argc, char *argv[])
 {
-	int prime; 
+	int prime;
 	
-	int in = generate_natural();
-	while (read(in, &prime, sizeof(int))) {
-		
+	int i = generateNumbers();
+
+	// Bản chất là đọc số đầu tiên từ pipe, in ra
+	while (read(i, &prime, sizeof(int))) {
 		printf("prime %d\n", prime); 
-		in = prime_filter(in, prime);
+		i = filterPrime(i, prime);
 	}
 
 	exit(0);
 }
+
+/*
+Mô tả quá trình:
+
+ 2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 ...
+ ↑
+ Đọc số đầu tiên -> prime 2 (In ra màn hình)
+ Lọc số chia hết cho 2:
+  3  5  7  9 11 13 15 17 ...
+    ↑
+    Đọc số đầu tiên -> prime 3 (In ra màn hình)
+    Lọc số chia hết cho 3:
+     5  7 11 13 17 ...
+       ↑
+       Đọc số đầu tiên -> prime 5 (In ra màn hình)
+       Lọc số chia hết cho 5:
+         7 11 13 17 ...
+           ↑
+           Đọc số đầu tiên -> prime 7 (In ra màn hình)
+           ...
+
+*/
